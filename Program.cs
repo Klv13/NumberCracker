0419738854de554c2f1e306bbf8a8d4fcbdb22e6
Klv13 <92885835+Klv13@users.noreply.github.com>
2022-01-30 15:40:01 +0100

    Upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace NumberCracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+
+            
+
+            Console.WriteLine("Enter passcode: ");
+
+            string Codes = Console.ReadLine();
+
+            int Code = int.Parse(Codes);
+
+            
+
+            int guess = 0;
+
+
+            while(guess != Code)
+            {
+                Console.WriteLine(guess);
+                guess++;
+       
+            }
+
+            Console.WriteLine("Guess: " + guess);
+            Console.WriteLine("\n\nIf the digits in gues != in Code just put 0s infront");
+
+            Console.WriteLine("The Actual input at start: " +Codes);
+
+            
+
+            
+
+            Console.ReadLine();
+            
+        }
+
+
+        
+    }
+}
